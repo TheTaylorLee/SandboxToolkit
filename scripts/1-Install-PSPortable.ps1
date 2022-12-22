817c17e67796956a1c522033944a8be41d7de1cb
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-12-21 18:28:49 -0600

    use light version

--- scripts/1-Install-PSPortable.ps1
+++ scripts/1-Install-PSPortable.ps1
@@ -3,6 +3,6 @@ Set-ExecutionPolicy Unrestricted -Confirm:$false -Force
 
 # Install PSPortable
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-(Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/master/Deploy-PSPortable.ps1 -UseBasicParsing).content | Invoke-Expression
+(Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/master/Deploy-PSPortableLight.ps1 -UseBasicParsing).content | Invoke-Expression
 install-psportable
 exit
