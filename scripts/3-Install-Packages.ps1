c8bda0e9751b08702eb2692758cb689d7135b714
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2022-12-23 08:47:11 -0600

    More shell writes

--- scripts/3-Install-Packages.ps1
+++ scripts/3-Install-Packages.ps1
@@ -52,33 +52,39 @@ https://github.com/TheTaylorLee/AdminToolbox
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Configure explorer view
+Write-Host "[+] Unhiding extensions, files/folders, and protected files" -ForegroundColor Green
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowSuperHidden /t REG_DWORD /d 1 /f
 taskkill.exe /im explorer.exe /f
 explorer.exe
+Write-Host "[+] Installing Fonts" -ForegroundColor Green
 
 # Install Fonts
 . "C:\ProgramData\PS7x64\PS7-x64\profile_snippets\font\Meslo LG M Regular Nerd Font Complete Mono.ttf"
 
 #Install Detect it Easy (DIE)
+Write-Host "[+] Downloading DIE - Detect it Easy" -ForegroundColor Green
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/horsicq/DIE-engine/releases/download/3.06/die_win64_qt6_portable_3.06.zip', "$env:userprofile\desktop\die_win64_qt6_portable_3.06.zip")
 Invoke-Unzip -zipfile $env:userprofile\desktop\die_win64_qt6_portable_3.06.zip -outpath "$env:userprofile\desktop\Detect it Easy"
 Remove-Item $env:userprofile\desktop\die_win64_qt6_portable_3.06.zip -Force
 
 # Install Retoolkit
 ##Using .net method to download file because it's quicker and works with github
+Write-Host "[+] Installing Retoolkit" -ForegroundColor Green
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/mentebinaria/retoolkit/releases/download/2022.10/retoolkit_2022.10_setup.exe', "$env:userprofile\downloads\retoolkit_2022.10_setup.exe")
 Start-Process $env:userprofile\downloads\retoolkit_2022.10_setup.exe -Wait
 Remove-Item $env:userprofile\Desktop\cmd.lnk -Force
 
 # Install Floss
+Write-Host "[+] Downloading Floss" -ForegroundColor Green
 (New-Object System.Net.WebClient).DownloadFile('https://github.com/mandiant/flare-floss/releases/download/v2.1.0/floss-v2.1.0-windows.zip', "$env:userprofile\desktop\floss-v2.1.0-windows.zip")
 Invoke-Unzip -zipfile $env:userprofile\desktop\floss-v2.1.0-windows.zip -outpath "$env:userprofile\desktop\Floss"
 Copy-Item $env:userprofile\desktop\Floss\floss.exe $env:systemroot\system32
 Remove-Item "$env:userprofile\desktop\floss-v2.1.0-windows.zip" -Force
 
 #Install Tools
+Write-Host "[+] Installing Google Chrome, Oh-My-Posh, Lockhunter, python, vscode, and wireshark" -ForegroundColor Green
 [string[]]$wingetlist = "Google Chrome", "JanDeDobbeleer.OhMyPosh", "lockhunter", "vscode", "wireshark"
 foreach ($install in $wingetlist) {
     winget install $install --accept-package-agreements --accept-source-agreements
@@ -90,10 +96,12 @@ Copy-Item "C:\Users\WDAGUtilityAccount\AppData\Roaming\Microsoft\Windows\Start M
 
 # Clone Repositories (Malwareoverview, ...)
 # Important if changing directories to run py scripts to then change back to the github root folder
+Write-Host "[+] Cloning malwareoverview" -ForegroundColor Green
 Set-Location "$env:userprofile\desktop\github"
 . "C:\Program Files\Git\bin\git.exe" clone https://github.com/alexandreborges/malwoverview
 
 # Instructs to set python as default app
+Write-Host "[+] Set Default Python App" -ForegroundColor Green
 Write-Host "
 You must set the file association for python right now.
     In the just opened explorer window right click a file with a .py extension and open it's properties.
@@ -104,9 +112,11 @@ cmd /c start %windir%\explorer.exe $env:userprofile\desktop\github\malwoverview
 Pause
 
 # Install pywhat
+Write-Host "[+] Installing pyWhat" -ForegroundColor Green
 . "C:\Python311\Scripts\pip3.exe" install pywhat
 
 # Install VPN
+Write-Host "[+] Installing nordvpn" -ForegroundColor Green
 Write-Warning "Wait for installs to complete then hit enter to continue through the pause!"
 Set-Location $env:userprofile\downloads
 curl.exe https://downloads.nordcdn.com/apps/windows/NordVPN/latest/NordVPNSetup.exe --output vpn.exe
@@ -118,9 +128,8 @@ Write-Host "Continue once vpn is connected"
 Pause
 
 # Closeing Statement
+Write-Host "[+] Completed after the Nordvpn installer and setup is done. All open windows can now be closed and tools used" -ForegroundColor Green
 Write-Host "
-    Completed after the Nordvpn installer and setup is done. All open windows can now be closed and tools used.
-
     To use malwoverview, open a new powershell window and run the following.
         Set-Location $env:userprofile\desktop\github\malwoverview
         .\setup.py build
