576edcb18977372c84b59e89be10d6b5fe357b71
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2024-01-25 09:49:38 -0600

    fix install-psportable

--- scripts/1-Install-PSPortable.ps1
+++ scripts/1-Install-PSPortable.ps1
@@ -5,7 +5,7 @@ Set-ExecutionPolicy Unrestricted -Confirm:$false -Force
 
 # Install PSPortable
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-(Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/master/Deploy-PSPortableLight.ps1 -UseBasicParsing).content | Invoke-Expression
+(Invoke-WebRequest https://raw.githubusercontent.com/TheTaylorLee/PSPortable/main/Deploy-PSPortableLight.ps1 -UseBasicParsing).content | Invoke-Expression
 Copy-Item "C:\ProgramData\PS7x64\PS7-x64\pwsh.exe.lnk" "$env:userprofile\desktop\pwsh.exe.lnk"
 taskkill.exe /im pwsh.exe /f
 exit
