09fc819c36dc2fe06504f9d31ea0c387af5d270c
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2024-01-25 12:20:11 -0600

    fix: update winget version

--- scripts/2-Install-PackageManagers.ps1
+++ scripts/2-Install-PackageManagers.ps1
@@ -4,7 +4,7 @@ Start-Transcript "$env:userprofile\desktop\logs\2-Install-PackageManagers.log"
 
 #Winget
 $ProgressPreference = 'Silent'
-(New-Object System.Net.WebClient).DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle', "$env:userprofile\downloads\MicrosoftDesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
+(New-Object System.Net.WebClient).DownloadFile('https://github.com/microsoft/winget-cli/releases/download/v1.6.3482/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle', "$env:userprofile\downloads\MicrosoftDesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
 (New-Object System.Net.WebClient).DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', "$env:userprofile\downloads\Microsoft.VCLibs.x64.14.00.Desktop.appx")
 Set-Location $env:userprofile\downloads
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
