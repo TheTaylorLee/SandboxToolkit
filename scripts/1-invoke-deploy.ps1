fe99e27aa132355d289767903813c311c8ab4913
TheTaylorLee <53202926+TheTaylorLee@users.noreply.github.com>
2025-11-03 11:55:42 -0600

    remove uneeded pause

--- scripts/1-invoke-deploy.ps1
+++ scripts/1-invoke-deploy.ps1
@@ -9,16 +9,7 @@ function Invoke-Deploy {
     2. Be patient some steps take longer than others. Allow for the script to finish running.
     3. When presented install wizards or install choices interaction is required.
     " -ForegroundColor Green
-    Pause
-
-    # This is legacy code from switching to using wsb sandbox config files on date 20251016. consider delting in the future.
-    # Clone repo and scripts for running
-    # Set-ExecutionPolicy Unrestricted -Confirm:$false -Force
-    # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    # New-Item $env:userprofile\desktop\Github -ItemType directory | Out-Null
-    # (New-Object System.Net.WebClient).DownloadFile('https://github.com/TheTaylorLee/SandboxToolkit/archive/refs/heads/main.zip', "$env:userprofile\desktop\github\SandboxToolkit.zip")
-    # Expand-Archive -Path $env:userprofile\desktop\github\SandboxToolkit.zip $env:userprofile\desktop\github\SandboxToolkit
-    # Remove-Item $env:userprofile\desktop\github\SandboxToolkit.zip -Force | Out-Null
+    Write-Host " "
 
     #Runs scripts
     Start-Process "powershell.exe" -ArgumentList "-executionpolicy unrestricted", "-File C:\temp\SandboxToolkit\scripts\2-Install-PackageManagers.ps1"
